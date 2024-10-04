5668c25b2ced78ef5516d39255adc5b4f30fc6b9
Andy Kreling <krelinga@gmail.com>
2024-10-04 05:06:11 +0000

    Register custom search attributes in test.
    Do this by calling the binary that the temporal test command fetches.
    
    Also, substantially speed up test time by remembering to stop the test server...

--- service_test.go
+++ service_test.go
@@ -2,22 +2,83 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 
 	pb "buf.build/gen/go/krelinga/proto/protocolbuffers/go/krelinga/video/tccoord/v1"
 	"connectrpc.com/connect"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.temporal.io/sdk/client"
 	temporal_testsuite "go.temporal.io/sdk/testsuite"
 )
 
+func getTemporalBinaryPath(dir string) (string, error) {
+	list, err := os.ReadDir(dir)
+	if err != nil {
+		return "", err
+	}
+	if len(list) != 1 {
+		return "", errors.New("expected exactly one file in directory")
+	}
+	return filepath.Join(dir, list[0].Name()), nil
+}
+
+func initCustomSearchAttribute(ctx context.Context, binaryPath, hostport string) error {
+	cmd := exec.CommandContext(ctx, binaryPath, "operator", "search-attribute", "create", "--name=dir", "--type=Keyword")
+	cmd.Env = append(os.Environ(), "TEMPORAL_ADDRESS="+hostport)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
+
+func getFreePort() (port int, err error) {
+	var a *net.TCPAddr
+	if a, err = net.ResolveTCPAddr("tcp", "localhost:0"); err == nil {
+		var l *net.TCPListener
+		if l, err = net.ListenTCP("tcp", a); err == nil {
+			defer l.Close()
+			return l.Addr().(*net.TCPAddr).Port, nil
+		}
+	}
+	return
+}
+
 func TestTcCoord(t *testing.T) {
+	tempDir, err := os.MkdirTemp("", "tc-coord-test")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempDir)
+
+	port, err := getFreePort()
+	require.NoError(t, err)
+	hostport := fmt.Sprintf("localhost:%d", port)
+
 	// Create a new service instance
-	devTempOpts := temporal_testsuite.DevServerOptions{}
+	devTempOpts := temporal_testsuite.DevServerOptions{
+		CachedDownload: temporal_testsuite.CachedDownload{
+			DestDir: tempDir,
+		},
+		ClientOptions: &client.Options{
+			HostPort: hostport,
+		},
+	}
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	devTemp, err := temporal_testsuite.StartDevServer(ctx, devTempOpts)
 	require.NoError(t, err)
+	defer devTemp.Stop()
+
+	temporalBinPath, err := getTemporalBinaryPath(tempDir)
+	require.NoError(t, err)
+	t.Log("temporal binary path:", temporalBinPath)
+	err = initCustomSearchAttribute(ctx, temporalBinPath, hostport)
+	require.NoError(t, err)
+
 	service := &tcCoord{
 		temporalClient: devTemp.Client(),
 	}
